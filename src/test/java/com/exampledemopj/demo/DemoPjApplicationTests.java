dd9fb8d91ee15b2c4748ca4aacb74a858f9e265a
prateek reddy mandadi <prateek.mandadi@prateeks-MacBook-Pro.local>
2023-10-17 09:28:52 -0400

    first commit

---
+++ src/test/java/com/exampledemopj/demo/DemoPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampledemopj.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
