5fb874cd683a9bd7e02c1ecca181888a27bff6f1
prateek251b <prateekm251@gmail.com>
2023-10-17 10:04:08 -0400

    second commit

--- src/main/java/com/exampledemopj/demo/DemoPjApplication.java
+++ src/main/java/com/exampledemopj/demo/DemoPjApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoPjApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoPjApplication.class, args);
+
+		System.out.println("Hello World");
 	}
+
 
 }
